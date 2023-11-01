cf26dd237512d7dc8ad532f82fec3444d6de7f6b
Shilpa <shilpa25988@gmail.com>
2023-11-01 20:59:27 +0000

    filemodified

--- practicePackage/PracticeClass.java
+++ practicePackage/PracticeClass.java
@@ -1,10 +1,13 @@
 package practicePackage;
 
+import java.sql.SQLOutput;
+
 public class PracticeClass {
 
     public static void main(String[] args) {
 
         System.out.println("What is Software Testing");
+        System.out.println(" what is manual Testing?");
     }
 
 }
